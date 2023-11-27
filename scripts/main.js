dfdb76ac602235e7b1ec1b57bd624e5671c54318
Aayush Rijal <arijal@aiims.com.au>
2023-11-28 10:16:16 +1100

    Phone validation done

--- scripts/main.js
+++ scripts/main.js
@@ -148,3 +148,22 @@ $(() => {
         }
     })
 });
+
+function validateForm() {
+    let phoneInput = $('#contactNumber').val();
+    let re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+
+    if(!re.test(phoneInput)) {
+        $('#phoneError').fadeIn();
+
+        return false;
+    } else {
+        $('#phoneError').fadeOut();
+    }
+
+    return true;
+}
+
+$("#form").on('submit', function () {
+    return validateForm();
+});
