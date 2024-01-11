b172aa585479753aa023a9ce810b412094417d43
Aayush Rijal <arijal@aiims.com.au>
2024-01-11 16:45:29 +1100

    Tracking update

--- app/thankyou.php
+++ app/thankyou.php
@@ -2,6 +2,14 @@
 
 <?php include __DIR__ . '/src/header.php'; ?>
 
+<!-- Event snippet for Quote Form (Roofing) conversion page -->
+
+<script>
+    gtag('event', 'conversion', {
+        'send_to': 'AW-11360914175/z4HQCNGFx4cZEP-Vp6kq'
+    });
+</script>
+
 <div class="thankyou-wrapper text-white text-center py-3" style="background-color: green;">
     Thank you! Your message has been sent. We will contact you shortly.
 </div>
